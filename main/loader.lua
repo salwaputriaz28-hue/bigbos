f8ba06b5333b62fdc1153333965a1cdf67a73276
salwaputriaz28-hue <salwaputriaz28@gmail.com>
2026-02-02 11:37:30 +0700

    Delete main directory

--- main/loader.lua
+++
@@ -1,5 +0,0 @@
--- BIGBOS V3 Loader | Delta Friendly
-if getgenv().BIGBOS_V3_LOADED then return end
-getgenv().BIGBOS_V3_LOADED = true
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/salwaputriaz28/BIGBOS/main/BIGBOS_V3.lua"))()
